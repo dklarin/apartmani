1eba806e9b8a63d3d4aa182953086c54e7232fad
dklarin <danijel.klarin@gmail.com>
2019-10-24 17:42:19 +0200

    Updates

---
+++ precache-manifest.ae0d8e091db1f2f5c35dc19793b7b16f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11aa7c0bbab2d84702cefe5b4c496d19",
+    "url": "/apartmani/index.html"
+  },
+  {
+    "revision": "8895b3c96548fba5f378",
+    "url": "/apartmani/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "0d8084d5ae1904e5240b",
+    "url": "/apartmani/static/js/2.b073f863.chunk.js"
+  },
+  {
+    "revision": "8895b3c96548fba5f378",
+    "url": "/apartmani/static/js/main.4cdb9148.chunk.js"
+  },
+  {
+    "revision": "cf782df4f5e2d878156f",
+    "url": "/apartmani/static/js/runtime-main.235530f2.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/apartmani/static/media/logo.25bf045c.svg"
+  }
+]);
